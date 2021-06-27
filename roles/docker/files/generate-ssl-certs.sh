cfdec01fc04ea221194f344e82f663d884318c56
ghibbo <ghibbo@gmail.com>
2021-06-27 19:18:58 +0200

    Generate Server and Client script. Test file.

---
+++ roles/docker/files/generate-ssl-certs.sh
@@ -0,0 +1,12 @@
+mkdir server-cert
+openssl genrsa -out ./server-cert/docker-daemon-server.key 1024
+openssl req -new -key ./server-cert/docker-daemon-server.key -x509 -days 180 -out ./server-cert/docker-daemon-server.crt
+cat ./server-cert/docker-daemon-server.key ./server-cert/docker-daemon-server.crt >./server-cert/docker-daemon-server.pem
+chmod 600 ./server-cert/docker-daemon-server.key ./server-cert/docker-daemon-server.pem
+
+mkdir client-certopenssl 
+genrsa -out ./client-cert/docker-daemon-client.key 1024
+openssl req -new -key ./client-cert/docker-daemon-client.key -x509 -days 180 -out ./client-cert/docker-daemon-client.crt
+cat ./client-cert/docker-daemon-client.key ./client-cert/docker-daemon-client.crt >./client-cert/docker-daemon-client.pem
+chmod 600 ./client-cert/docker-daemon-client.key ./client-cert/docker-daemon-client.pem
+
